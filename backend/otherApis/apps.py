bb332bb7cea0f9371345a01f1058e1f6b4eba7ba
JonFactor <111780795+JonFactor@users.noreply.github.com>
2024-04-13 21:54:06 -0400

    56

--- backend/otherApis/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OtherapisConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'otherApis'
--- backend/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import UserView, LoginView
-
-urlpatterns = [ 
-     path("user/", UserView.as_view()),
-     path("login/", LoginView.as_view()) ]
